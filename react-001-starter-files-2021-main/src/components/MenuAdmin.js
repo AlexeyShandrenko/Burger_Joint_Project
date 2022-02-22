a09165f332895c1e095c7d12083dfb3f20b3a057
AlexeyShandrenko <70972424+AlexeyShandrenko@users.noreply.github.com>
2022-02-22 14:07:45 +0300

    state in App component

--- react-001-starter-files-2021-main/src/components/MenuAdmin.js
+++ react-001-starter-files-2021-main/src/components/MenuAdmin.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React from "react";
+import AddBurgerForm from "./AddBurgerForm";
 
 class MenuAdmin extends React.Component {
-    render() {
-        return(
-            <div className="menu-admin">
-                <h2>Управление Меню</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="menu-admin">
+        <h2>Управление Меню</h2>
+        <AddBurgerForm addBurger={this.props.addBurger}/>
+      </div>
+    );
+  }
 }
 
-export default MenuAdmin
+export default MenuAdmin;
